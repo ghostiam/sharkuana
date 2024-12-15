9f435c836fd0daeb7e75ca2bea5c5b33d8c2b942
Vladislav Fursov <github@ghost.im>
2024-12-15 22:34:41 +0400

    rewrite minimal plugin example without cImport

--- build.zig
+++ build.zig
@@ -5,53 +5,58 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const options = .{};
+    // const sharkuana_module = b.addModule("root", .{
+    //     .root_source_file = b.path("src/sharkuana.zig"),
+    //     .optimize = optimize,
+    //     .target = target,
+    // });
+    // addLibsTo(b, sharkuana_module, target.result);
+    // sharkuana_module.linkSystemLibrary("glib-2.0", .{});
 
-    const options_step = b.addOptions();
-    inline for (std.meta.fields(@TypeOf(options))) |field| {
-        options_step.addOption(field.type, field.name, @field(options, field.name));
-    }
-
-    const options_module = options_step.createModule();
-
-    const m = b.addModule("root", .{
+    const sharkuana_lib = b.addSharedLibrary(.{
+        .name = "sharkuana",
         .root_source_file = b.path("src/sharkuana.zig"),
-        .imports = &.{
-            .{ .name = "sharkuana_options", .module = options_module },
-        },
+        .target = target,
         .optimize = optimize,
     });
-    addLibsTo(b, m, target.result);
-    m.addIncludePath(b.path("src"));
+    addLibraryPathsTo(sharkuana_lib);
+    b.installArtifact(sharkuana_lib);
+
+    // ZLS magic: https://zigtools.org/zls/guides/build-on-save/
+    const sharkuana_lib_check = b.addSharedLibrary(.{
+        .name = "sharkuana_check",
+        .root_source_file = b.path("src/sharkuana.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const check_step = b.step("check", "Check if foo compiles");
+    check_step.dependOn(&sharkuana_lib_check.step);
 }
 
 pub fn addLibraryPathsTo(compile_step: *std.Build.Step.Compile) void {
     const b = compile_step.step.owner;
     const target = compile_step.rootModuleTarget();
-
     addLibsTo(b, compile_step, target);
+
+    compile_step.linkSystemLibrary("glib-2.0");
 }
 
 pub fn addLibsTo(b: *std.Build, compile_step: anytype, target: std.Target) void {
     const ws_dep = b.dependency("wireshark", .{});
 
-    compile_step.addIncludePath(ws_dep.path(""));
-    compile_step.addIncludePath(ws_dep.path("include"));
-    compile_step.addIncludePath(ws_dep.path("epan"));
-    compile_step.addIncludePath(ws_dep.path("wsutil"));
-
-    // TODO: generate ws_version.h file and put in plugin include path instead build wireshark libs?
-    compile_step.addIncludePath(ws_dep.path("build"));
-
     switch (target.os.tag) {
         .macos => {
-            // TODO: glib as dependency?
-            compile_step.addIncludePath(.{ .cwd_relative = "/opt/homebrew/lib/glib-2.0/include" }); // contains glibconfig.h
-            compile_step.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include/glib-2.0" });
-
             compile_step.addObjectFile(ws_dep.path("build/run/libwireshark.dylib"));
             compile_step.addObjectFile(ws_dep.path("build/run/libwsutil.dylib"));
         },
-        else => {},
+        .linux, .freebsd => {
+            compile_step.addObjectFile(ws_dep.path("build/run/libwireshark.so"));
+            compile_step.addObjectFile(ws_dep.path("build/run/libwsutil.so"));
+        },
+        .windows => {
+            compile_step.addObjectFile(ws_dep.path("build/run/libwireshark.dll"));
+            compile_step.addObjectFile(ws_dep.path("build/run/libwsutil.dll"));
+        },
+        else => @panic("unsupported OS"),
     }
 }
--- src/sharkuana.zig
+++ src/sharkuana.zig
@@ -1,152 +1,38 @@
-const root = @import("root");
 const std = @import("std");
-const c = @cImport({
-    @cInclude("epan/packet.h");
-});
+const log = std.log.scoped(.sharkuana);
 
-export const plugin_version: [root.wiresharkPlugin.version.len:0]u8 = cast_anytype_ptr([root.wiresharkPlugin.version.len:0]u8, root.wiresharkPlugin.version).*;
-export const plugin_want_major: c_int = c.WIRESHARK_VERSION_MAJOR;
-export const plugin_want_minor: c_int = c.WIRESHARK_VERSION_MINOR;
-
-pub fn cast_anytype_ptr(comptime T: type, ptr: anytype) *T {
-    return @constCast(@ptrCast(@alignCast(ptr)));
-}
-
-pub const Plugin = struct {
-    version: [:0]const u8,
-    plugin_describe: PluginDesc,
-    protocol: PluginProtocol,
-    dissector: PluginDissector,
+// nix develop -c zig build && cp zig-out/lib/libsharkuana.dylib ~/.local/lib/wireshark/plugins/4-4/epan/libsharkuana.so && /Applications/Wireshark.app/Contents/MacOS/Wireshark --log-domains sharkuana --log-level noisy
+pub const std_options = std.Options{
+    // Set wireshark logger.
+    .logFn = wsLogger,
 };
 
-pub const PluginProtocol = struct {
-    name: [:0]const u8,
-    short_name: [:0]const u8,
-    filter_name: [:0]const u8, // TODO check format in comptime
-};
+const version = "1.2.3";
 
-pub const PluginDissector = struct {
-    // TODO: use create_dissector_handle_with_name_and_description
-    name: ?[*:0]const u8,
-    description: ?[*:0]const u8,
-    handler: PluginDissectorFn,
-};
+pub export const plugin_version: [version.len:0]u8 = version.*;
+pub export const plugin_want_major: c_int = 4;
+pub export const plugin_want_minor: c_int = 4;
 
-pub const DissectorError = error{};
-
-pub const PluginDissectorFn = *const fn (*TvBuff, *PacketInfo, *ProtoTree, Proto) DissectorError!i32;
-
-pub const PluginDesc = enum(u32) {
-    Dissector = 1 << 0,
-    FileType = 1 << 1,
-    Codec = 1 << 2,
-    Epan = 1 << 3,
-    TapListener = 1 << 4,
-    DFilter = 1 << 5,
-};
-
-export fn plugin_describe() u32 {
-    return @intFromEnum(root.wiresharkPlugin.plugin_describe);
+fn proto_register_zig() void {
+    const proto_my_plugin = proto_register_protocol("Wireshark Hello Plugin ZIG", "Hello WS ZIG", "hello_ws_zig");
+    log.debug("Proto: {}", .{proto_my_plugin});
 }
+extern fn proto_register_protocol(name: [*:0]const u8, short_name: [*:0]const u8, filter_name: [*:0]const u8) c_int;
 
-pub var registeredProto: Proto = undefined;
+pub export fn plugin_register() void {
+    log.info("Hello from ZIG plugin_register", .{});
 
-fn proto_register() void {
-    wsLogger(.debug, .sharkuana, "proto_register_zig", .{});
-
-    const wp: Plugin = root.wiresharkPlugin;
-
-    registeredProto = proto_register_protocol(wp.protocol.name, wp.protocol.short_name, wp.protocol.filter_name);
-
-    // var h = create_dissector_handle(wrapped_dissector, registeredProto.id);
-    var h = create_dissector_handle_with_name_and_description(wrapped_dissector, registeredProto.id, wp.dissector.name, wp.dissector.description);
-    h.registerPostdissector();
-
-    const m = prefs_register_protocol(registeredProto, empty);
-    c.prefs_register_static_text_preference(m, "helper", "Test sharkuana prefs", null);
-}
-
-fn empty() callconv(.C) void {
-    wsLogger(.debug, .sharkuana, "empty", .{});
-}
-
-pub const PrefsApplyFn = *const fn () callconv(.C) void;
-
-extern fn prefs_register_protocol(proto: Proto, apply_cb: PrefsApplyFn) *c.module_t;
-
-fn wrapped_dissector(tvb: *TvBuff, pinfo: *PacketInfo, tree: *ProtoTree, _: ?*anyopaque) callconv(.C) i32 {
-    wsLogger(.debug, .sharkuana, "wrapped_dissector", .{});
-
-    const wp: Plugin = root.wiresharkPlugin;
-
-    return wp.dissector.handler(tvb, pinfo, tree, registeredProto) catch |err| {
-        wsLogger(.err, .sharkuana, "wrapped_dissector error: {}", .{err});
-    };
-}
-
-////////////
-
-export fn plugin_register() void {
-    wsLogger(.debug, .sharkuana, "Register plugin", .{});
-
-    const plug = proto_plugin{
-        .register_protoinfo = proto_register,
+    const plug: proto_plugin = proto_plugin{
+        .register_protoinfo = proto_register_zig,
         .register_handoff = null,
-        // .register_protoinfo = root.proto_register,
-        // .register_handoff = if (@hasDecl(root, "proto_reg_handoff")) root.proto_reg_handoff else null,
     };
     proto_register_plugin(&plug);
 }
-
-pub const TvBuff = opaque {
-    pub fn capturedLength(tvb: *TvBuff) u32 {
-        return tvb_captured_length(tvb);
-    }
-    extern fn tvb_captured_length(tvb: *TvBuff) c_uint;
-};
-
-pub const PacketInfo = extern struct { current_proto: *const u8, cinfo: *c.epan_column_info, presence_flags: u32, num: u32, abs_ts: std.posix.timeval };
-
-pub const ProtoTree = opaque {
-    pub fn AddProtocolFormat(tree: *ProtoTree, fieldIndex: i32, tvb: *TvBuff, start: c_int, length: c_int, comptime format: []const u8, args: anytype) !*c.proto_item {
-        const alloc = std.heap.page_allocator; // TODO: extract allocator.
-        const msg = try std.fmt.allocPrintZ(alloc, format, args);
-        defer alloc.free(msg);
-
-        return proto_tree_add_protocol_format(tree, fieldIndex, tvb, start, length, msg);
-    }
-    extern fn proto_tree_add_protocol_format(tree: *ProtoTree, hfindex: i32, tvb: *TvBuff, start: c_int, length: c_int, format: [*:0]const u8, ...) *c.proto_item;
-};
-
-const proto_plugin = extern struct {
+pub const proto_plugin = extern struct {
     register_protoinfo: ?*const fn () void,
-    register_handoff: ?*const fn () void,
+    register_handoff: ?*const fn () void = null,
 };
-extern fn proto_register_plugin(plugin: *const proto_plugin) void;
-
-pub const DissectorFn = *const fn (*TvBuff, *PacketInfo, *ProtoTree, ?*anyopaque) callconv(.C) i32;
-
-pub const DissectorHandle = opaque {
-    pub fn registerPostdissector(handle: *DissectorHandle) void {
-        register_postdissector(handle);
-    }
-};
-extern fn register_postdissector(handle: *DissectorHandle) void;
-
-pub const Proto = extern struct {
-    id: i32,
-
-    pub fn createDissectorHandle(proto: *Proto, dissector: DissectorFn) *DissectorHandle {
-        return create_dissector_handle(dissector, proto.id);
-    }
-};
-extern fn create_dissector_handle(dissector: DissectorFn, proto: c_int) *DissectorHandle;
-extern fn create_dissector_handle_with_name_and_description(dissector: DissectorFn, proto: c_int, name: ?[*:0]const u8, description: ?[*:0]const u8) *DissectorHandle;
-
-pub fn protoRegisterProtocol(name: [*:0]const u8, short_name: [*:0]const u8, filter_name: [*:0]const u8) Proto {
-    return proto_register_protocol(name, short_name, filter_name);
-}
-extern fn proto_register_protocol(name: [*:0]const u8, short_name: [*:0]const u8, filter_name: [*:0]const u8) Proto;
+extern fn proto_register_plugin(plugin: [*c]const proto_plugin) void;
 
 // Logger
 
